1e75e1bc352c72cf71f38708a3f8742e00f37a58
Osman Çağrı GENÇ <ocgenc@gmail.com>
2021-06-24 10:48:09 +0300

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
-// if (window.location.protocol === 'http:') {
+if (window.location.protocol === 'http:') {
     
-//     location.href = window.location.href.replace('http://', 'https://');
-// }
+     location.href = window.location.href.replace('http://', 'https://');
+}
 
 const socket = io("/"); //getting dependency
 socket.emit("initialize",url); //called every time new user joins the room to initialze the notepad
