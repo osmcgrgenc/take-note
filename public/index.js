3698ea51a9047adac9fc6f80f76338018dcf1fa7
Osman Çağrı GENÇ <ocgenc@gmail.com>
2021-06-24 14:27:44 +0300

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,11 @@
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+const userName = uuidv4();
 if (window.location.protocol === 'http:') {
     
      location.href = window.location.href.replace('http://', 'https://');
